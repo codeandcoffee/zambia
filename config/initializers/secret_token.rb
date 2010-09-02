4d8679c0bb1ab927292ae42956e989a6497fa9c5
Tim Wingfield <tim@timwingfield.com>
2010-09-02 07:39:16 -0400

    Initial commit of zambia rails project for Code and Coffee blog idear

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zambia::Application.config.secret_token = 'c4a90c1a99494219357c8c49835970af75c46bf8827556568d438c2b2eb2d0f4ac78b41701d8a6c5f49537ad064ac31aa00a0c60f3961ced7f6ba94f0c56848a'
