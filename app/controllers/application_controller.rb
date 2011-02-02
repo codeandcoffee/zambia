461d9ff577fda05cf577d8a91601f1e4b6e5b958
Shawn Wallace <shawnewallace@gmail.com>
2011-02-02 15:12:46 -0500

    removed reference to omniauth from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  helper_method :current_user
-  
-  private
-  
-  def current_user
-    @current_user ||= User.find(:first, :conditions => { :id => session[:user_id]}) if session[:user_id]
     
-    #raise @current_user.to_yaml if @current_user
-  end
+  
 end
