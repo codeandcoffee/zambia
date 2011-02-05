6e5e6ddcf27d7e587ad0c78157096eb6d33fa7d6
Shawn Wallace <shawnewallace@gmail.com>
2011-02-05 16:07:36 -0500

    added method to location to validate address enough to generate a map url

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -19,4 +19,26 @@ describe Location do
   it { should have_field(:time) }
   
   it { should validate_presence_of(:name) }
+
+  describe "when checking if the location has a good address" do
+    it 'should be falls when new' do
+      instance = Location.new
+      instance.goodAddress?.should == false
+    end
+
+    [
+      [nil, nil, false],
+      ["GOOD STREET", nil, false],
+      [nil, "GOOD CITY", false],
+      ["GOOD STREET", "GOOD CITY", true]
+    ].each do |street1, city, expected|
+      it "should be #{expected} when street1 is '#{street1}' and city is '#{city}'" do
+        instance = Location.new
+        instance.street1 = street1
+        instance.city = city
+        instance.goodAddress?.should == expected
+      end
+    end
+  end
 end
+
