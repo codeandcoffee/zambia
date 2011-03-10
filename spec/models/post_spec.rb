641c65e85dfcbf77d8d874b86c9c356ae3c05868
Tim Wingfield <tim@timwingfield.com>
2011-03-10 08:36:53 -0500

    added post spec. removed generated specs for helper files

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,4 +10,14 @@ describe Post do
   it { should validate_presence_of(:author) }
   it { should validate_presence_of(:title) }
   #it { should validates_uniqueness_of(:slug) }
+
+  describe "adding a new post" do
+    it "should take the first 250 char and make the intro" do
+      pending "add the character count spec"
+    end
+
+    it "should add an ellipsis to the end of the intro" do
+      pending "add the ellipsis"
+    end
+  end
 end
