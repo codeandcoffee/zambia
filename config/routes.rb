5c97fca64c5734c64fb9f299529b3eaf7b06b10b
Justin Searls <searls@gmail.com>
2011-10-21 15:22:48 -0400

    no specs in prod

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ TestDouble::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 
-  mount JasmineRails::Engine => "/specs"
+
+  mount JasmineRails::Engine => "/specs" unless Rails.env.production?
 
 end
