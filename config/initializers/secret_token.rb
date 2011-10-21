9b0e6e5d96ef82193b94a75ee9ae33e15ebf5f1d
Justin Searls <searls@gmail.com>
2011-10-21 14:21:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestDouble::Application.config.secret_token = 'fdc4c893237439de8f4a59c1232f1a57afaf463e7dccad09e46d7ea7d5de30a15a480d856b876513555e9a1300ba1b0f05c114818393228a6fafeaad8f7cb87d'
