4241fa854ea0b8af47087b0b1ded2b3d836d0389
Justin Searls <searls@gmail.com>
2011-10-21 15:10:11 -0400

    added jasmine-rails specs

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,7 @@ TestDouble::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  mount JasmineRails::Engine => "/specs"
+
 end
