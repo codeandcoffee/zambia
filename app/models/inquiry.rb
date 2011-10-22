8a552bf047fa0dcb0dd26364563ece3861037cff
Justin Searls <searls@gmail.com>
2011-10-22 12:45:54 -0400

    a way to deal with inbound camelCase parameters

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,15 @@
 class Inquiry < ActiveRecord::Base
+  def initialize(attributes = nil, options = {})
+    super underscore_keys(attributes), options
+  end
+
+  def attributes=(new_attributes,guard_protected_attributes=nil)
+    super underscore_keys(new_attributes)
+  end
+
+  private
+
+  def underscore_keys(hash)
+    Hash[hash.map { |k,v| [k.to_s.underscore.to_sym,v] }]
+  end
 end
