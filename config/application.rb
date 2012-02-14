e630452b9914fc9e908aeec179d554e6b23a8aa7
Justin Searls <searls@gmail.com>
2012-02-14 17:32:14 -0500

    don't wrap json

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,8 @@ module TestDouble
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # don't wrap model names in JSON
+    ActiveRecord::Base.include_root_in_json = false
   end
 end
