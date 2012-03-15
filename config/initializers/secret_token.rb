e6d4b140fef2246b5a0e1e38764cd639766789d7
Tim Wingfield <tim@timwingfield.com>
2012-03-15 07:40:50 -0400

    wiped it all out one more time to just steal from Searls

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Zambia::Application.config.secret_token = '7425798e2439e7ea21d39736ab34f6e0d43ea14b1316be3403bc363111247cc5e78eb98073cc8e6f3f3ded1315d031c77e58d3b736c4556abedbbd596a4d367b'
