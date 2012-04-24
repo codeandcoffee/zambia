c435cf423514e3db462ed66569dfbb81a8aab040
Tim Wingfield <tim@timwingfield.com>
2012-04-24 14:34:02 -0400

    added popovers for the maps

--- vendor/assets/javascripts/vendor.js
+++ vendor/assets/javascripts/vendor.js
@@ -1,4 +1,7 @@
 //= require underscore-min
 //= require jquery-min
 //= require backbone-min
+//= require bootstrap-alerts
+//= require bootstrap-tooltip
+//= require bootstrap-popover
 //= require_tree .
