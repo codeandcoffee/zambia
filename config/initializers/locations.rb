a45ddd0332aa6e6cc05ab5bea6f6fdaf02423c32
Tim Wingfield <tim@timwingfield.com>
2012-04-24 17:23:16 -0400

    finished the map popover. added a pg database

--- config/initializers/locations.rb
+++ config/initializers/locations.rb
@@ -3,17 +3,20 @@ LOCATIONS = [
     :coffeeshop => "Stauf's", 
     :day => "Thursday", 
     :time => "7:00", 
-    :map => "http://maps.google.com/maps?q=1277+Grandview+Avenue,+OH,+43212&hl=en&ll=39.985604,-83.044968&spn=0.045969,0.075703&hnear=1277+Grandview+Ave,+Columbus,+Franklin,+Ohio+43212&t=m&z=14&iwloc=A&output=embed" },
+    :map => "http://maps.google.com/maps?q=1277+Grandview+Avenue,+OH,+43212&hl=en&ll=39.985604,-83.044968&spn=0.045969,0.075703&hnear=1277+Grandview+Ave,+Columbus,+Franklin,+Ohio+43212&t=m&z=14&iwloc=A&output=embed",
+    :id => "cbus-staufs" }, 
 
   { :city => "Chicago", 
     :coffeeshop => "Starbucks Chicago & Franklin", 
     :day => "Tuesday", 
     :time => "7:00", 
-    :map => "http://maps.google.com/maps?q=750+North+Franklin+Street,+IL,+60654&hl=en&ll=41.89755,-87.635708&spn=0.044657,0.075703&hnear=750+N+Franklin+St,+Chicago,+Illinois+60654&t=m&z=14&output=embed" },
+    :map => "http://maps.google.com/maps?q=750+North+Franklin+Street,+IL,+60654&hl=en&ll=41.89755,-87.635708&spn=0.044657,0.075703&hnear=750+N+Franklin+St,+Chicago,+Illinois+60654&t=m&z=14&output=embed",
+    :id => "chi-sbux" },
 
   { :city => "London", 
     :coffeeshop => "Costa Coffee Sheperds Bush", 
     :day => "Friday", 
     :time => "7:30", 
-    :map => "http://maps.google.com/maps?q=Costa+Coffee+Shepherds+Bush,+UK,+W12+8LP&hl=en&ll=51.504896,-0.219662&spn=0.009336,0.018926&hq=Costa+Coffee+Shepherds+Bush,+UK,+W12+8LP&radius=15000&t=m&z=16&output=embed" },
+    :map => "http://maps.google.com/maps?q=Costa+Coffee+Shepherds+Bush,+UK,+W12+8LP&hl=en&ll=51.504896,-0.219662&spn=0.009336,0.018926&hq=Costa+Coffee+Shepherds+Bush,+UK,+W12+8LP&radius=15000&t=m&z=16&output=embed",
+    :id => "london-costa" },
 ]
